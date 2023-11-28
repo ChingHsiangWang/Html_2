ff6ef54527fd799d4423702d6e725ada662024ae
Chinghsiang Wang <s93906@gmail.com>
2023-11-28 13:38:55 +0800

    1st version

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*eslint-env browser*/
+var arrow_left = document.getElementById('arrow_left');
+arrow_left.onclick = function () {
+    'use strict';
+    document.getElementById('img_1').id = 'img_2';
+};
